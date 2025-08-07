558f8923ce4a4a04b77711b53978664b6ba33d4d
Olayinka <121318364+yinka8657@users.noreply.github.com>
2025-08-07 16:04:55 +0100

    Update API to use process.env.PORT for Render deployment

--- foodeck-api/index.js
+++ foodeck-api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Allow CORS so React can connect
 app.use(cors());
